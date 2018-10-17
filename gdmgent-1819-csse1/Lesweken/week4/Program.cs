7880cf73a75f138dcb0b5773355d49df5b7f62a1
gdm-1718-camivand4 <camivand4@student.arteveldehs.be>
2018-10-17 11:00:38 +0200

    gaonline

--- gdmgent-1819-csse1/Lesweken/week4/Program.cs
+++ gdmgent-1819-csse1/Lesweken/week4/Program.cs
@@ -1,32 +1,51 @@
 ﻿using System;
 
-namespace week4
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            int resultaat = 
-            Optellen(Optellen(Vermenigvuldigen(Optellen(100, 7), 3), Vermenigvuldigen(Delen(Optellen(406, 7367), 69), 5)), 
-            Optellen(Aftrekken(Vermenigvuldigen(73, 41), 39), Aftrekken(Vermenigvuldigen(7, 8) , 19)));
+namespace week4 {
+    class Program {
+        static void Main(string[] args) {
+
+            int resultaat =
+                Optellen(Optellen(Vermenigvuldigen(Optellen(100, 7), 3), Vermenigvuldigen(Delen(Optellen(406, 7367), 69), 5)),
+                    Optellen(Aftrekken(Vermenigvuldigen(73, 41), 39), Aftrekken(Vermenigvuldigen(7, 8), 19)));
 
             Console.WriteLine("Het resultaat is: " + resultaat);
 
-            int Optellen (int _a, int _b) {
+             int resultaat2 =
+                Optellen(Optellen(Vermenigvuldigen(Optellen(100, 8), 3), Vermenigvuldigen(Delen(Optellen(406, 7367), 69), 5)),
+                    Optellen(Aftrekken(Vermenigvuldigen(73, 41), 39), Aftrekken(Vermenigvuldigen(7, 8), 19)));
+
+            Console.WriteLine("Het resultaat is: " + resultaat2);
+
+            int Optellen(int _a, int _b) {
                 return _a + _b;
             }
 
-            int Aftrekken (int _a, int _b) {
+            int Aftrekken(int _a, int _b) {
                 return _a - _b;
             }
 
-            int Vermenigvuldigen (int _a, int _b) {
+            int Vermenigvuldigen(int _a, int _b) {
                 return _a * _b;
             }
 
-            int Delen (int _a, int _b) {
+            int Delen(int _a, int _b) {
                 return _a / _b;
+            }
+
+            if (resultaat == 3872) {
+                Console.WriteLine("het resultaat is perfect!");
+            } 
+            else if (resultaat > 3872)
+            {
+                Console.WriteLine("Het resultaat is niet correct!");
+            }
+
+            if (resultaat2 == 3872) {
+                Console.WriteLine("het resultaat is perfect!");
+            } 
+            else if (resultaat2 > 3872)
+            {
+                Console.WriteLine("Het resultaat is niet correct!");
             }
         }
     }
