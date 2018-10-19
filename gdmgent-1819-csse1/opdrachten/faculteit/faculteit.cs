4e339e4ff2e4ec5be1bd5837c3e220ec413a70e5
gdm-1718-camivand4 <camivand4@student.arteveldehs.be>
2018-10-19 16:16:54 +0200

    opdracht fibonacci en faculteit

---
+++ gdmgent-1819-csse1/opdrachten/faculteit/faculteit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace opdracht2 {
+    class Program {
+
+        static void Main(string[] args) {
+int fact = 5;
+int  x = 1;
+for (int i = 1; i <= fact; i++)
+{
+    x = x * i;
+}
+Console.WriteLine("de faculteit van " + fact + " is: " + x);
+    }
+}
+}
