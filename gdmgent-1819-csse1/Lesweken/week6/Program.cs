6372e8fb3f01506b756e73abe6ab64b0b94a9347
gdm-1718-camivand4 <camivand4@student.arteveldehs.be>
2018-11-08 16:56:46 +0100

    opdracht2

--- gdmgent-1819-csse1/Lesweken/week6/Program.cs
+++ gdmgent-1819-csse1/Lesweken/week6/Program.cs
@@ -1,14 +1,44 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace week6
+namespace ConsoleApplication
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-             string getal = Console.ReadLine();
+            var genes = Gene.GetArrayOfGenes();
+            string [] jamjam = new string[]
+            {
+                "Plat Water",
+                "Cola 25 Cl",
+                "Cola 33 Cl",
+                "Witte Wijn",
+                "Pils",
+                "Toast",
+                "Kaasplank"
+            };
+            
 
+            var orderedJamjam = jamjam.OrderBy(s => s.Length);
+            foreach(var f in genes)
+            {
+                Console.WriteLine(f);
+            }
+
+            Console.WriteLine(" ");
+
+            var orderedGenes = genes.OrderBy(g => g.Prijs);
+            foreach(var f in orderedGenes)
+            {
+                Console.WriteLine(f);
+            } 
+
+            Console.WriteLine("");
+            Console.WriteLine("Welke Dish wil je aanspreken?");
+            string aanspreekbareDish = Console.ReadLine();
         }
     }
 }
